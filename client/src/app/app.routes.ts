fb9574bd59b1959c8a219be9ce0484f97fbf1bd3
Vance McGrady <vancemcgrady@gmail.com>
2026-08-25 11:22:24 -0700

    change home to dashboard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', component: Dashboard },
   { path: '**', redirectTo: '' },
 ];
